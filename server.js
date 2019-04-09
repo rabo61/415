23116268e75938e6927e7fbe392ffc61e566e53d
robert <robertwaguespack@yahoo.com>
2019-04-08 21:32:31 -0500

    setup

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ var express = require('express');
 var chalk = require('chalk');
 var app = express();
 var router = express.Router();
+var port = process.env.PORT || 3000;
 
 router.get('/test', function(req, res) {
     res.status(200).send('Hello world');
 });
 
+app.get("/", function(req, res) {
+    res.send("welcome");
+});
+
 app.use('/api', router);
 
-app.listen(1069, function(err) {
+app.listen(port, function(err) {
     if (err) {
         console.log(chalk.red(err));
     } else {
